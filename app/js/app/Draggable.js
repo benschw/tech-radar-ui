7fe1edf155254f9234566ed118e0483f1c5d72e9
Ben Schwartz <benschw@gmail.com>
2016-03-04 10:00:55 -0600

    got directive working!

--- app/js/app/Draggable.js
+++ app/js/app/Draggable.js
@@ -1,85 +1,68 @@
 'use strict';
 
 goog.provide('demo.app.DraggableFactory');
-goog.provide('demo.app.Draggable');
 
 
 /**
- * Draggable Directive
- * @param  {Document} $document (not defined in externs)
- * @constructor
+ * Draggable Directive Factory
+ * @param  {angular.JQLite} $document (not defined in externs)
  * @ngInject
  */
 demo.app.DraggableFactory = function($document) {
-	return new demo.app.Draggable($document);
-};
-/**
- * Draggable Directive
- * @param  {Document} $document (not defined in externs)
- * @constructor
- */
-demo.app.Draggable = function($document) {
-	this.msg = "hello";
+
 	/**
-	 * @export
+	 * Draggable Directive
+	 * @param {angular.Scope} scope
+	 * @param {angular.JQLite} element
+	 * @param {angular.Attributes} attr
 	 */
-	this.doc = $document;
-	console.log([this.msg, this.doc]);
-};
+	var draggable = function(scope, element, attr) {
 
-/**
- * @param {angular.Scope} scope
- * @param {angular.JQLite} element
- * @param {angular.Attributes} attr
- * @export
- */
-demo.app.Draggable.prototype.link = function(scope, element, attr) {
+		var d = $document;
 
-	var d = element
+		var startX = 0;
+		var startY = 0;
+		var x = 0;
+		var y = 0;
 
-	console.log([this.msg, this.doc, element]);
-	var startX = 0;
-	var startY = 0;
-	var x = 0;
-	var y = 0;
-
-	element.css({
-		position: 'absolute',
-		cursor: 'pointer'
-	});
-
-	element.on('mousedown', function(event) {
-		if (!scope.editable) {
-			return;
-		}
-		// Prevent default dragging of selected content
-		event.preventDefault();
-		startX = event.pageX - x;
-		startY = event.pageY - y;
-		d.on('mousemove', mousemove);
-		d.on('mouseup', mouseup);
-	});
-
-	var mousemove = function(event) {
-		y = event.pageY - startY;
-		x = event.pageX - startX;
-
-		transform();
-	};
-	var transform = function() {
 		element.css({
-			transform: 'translate('+x+'px,'+y+'px)',
-			WebkitTransform: 'translate('+x+'px,'+y+'px)'
+			position: 'absolute',
+			cursor: 'pointer'
 		});
-		element.css('transform');
+
+		element.on('mousedown', function(event) {
+			if (!scope.editable) {
+				return;
+			}
+			// Prevent default dragging of selected content
+			event.preventDefault();
+			startX = event.pageX - x;
+			startY = event.pageY - y;
+			d.on('mousemove', mousemove);
+			d.on('mouseup', mouseup);
+		});
+
+		var mousemove = function(event) {
+			y = event.pageY - startY;
+			x = event.pageX - startX;
+
+			transform();
+		};
+		var transform = function() {
+			element.css({
+				transform: 'translate('+x+'px,'+y+'px)',
+				WebkitTransform: 'translate('+x+'px,'+y+'px)'
+			});
+			element.css('transform');
+		};
+		var mouseup = function() {
+			scope.radar.updateLocation(attr['myDraggable'], x, y);
+			x = 0;
+			y = 0;
+			transform();
+			d.off('mousemove', mousemove);
+			d.off('mouseup', mouseup);
+		};
 	};
-	var mouseup = function() {
-		scope.radar.updateLocation(attr['myDraggable'], x, y);
-		x = 0;
-		y = 0;
-		transform();
-		d.off('mousemove', mousemove);
-		d.off('mouseup', mouseup);
-	};
-	
+	return draggable;
 };
